b6868397d7a91cfa5b1647bfa35e5d554c8aaa52
Tangmouo Manfo, Brice <144279774+btmanfo@users.noreply.github.com>
2026-03-29 11:24:19 -0400

    Add: top-k-elements-in-list - submission-7

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-7.py
@@ -0,0 +1,23 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        #1 i make a dictionnary with key as num and value as frequency
+        # O(n) time complexty and O(n) space complexity
+        elementFreq = {}
+        for i in nums:
+            elementFreq[i] = 1 + elementFreq.get(0, i)
+
+        #2 make a table of tuple with tuple[0] num and tuple[1] frequency
+        # O(n) time complexiyy and O(n) space complexity
+        arrayTuple = []
+        for i,j in elementFreq.items():
+            arrayTuple.append((i,j))
+        
+        #3 Sort the array base on tuple[1]
+        # O(nlogn) time complexity
+        arrayTuple.sort(key = lambda x:x[1], reverse = True)
+
+        #4 return value
+        value = []
+        for i in range(k):
+            value.append(arrayTuple[i][0])
+        return value

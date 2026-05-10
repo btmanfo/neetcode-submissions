c97e1737e867fb523fcdf0cfca5e063fbaee5a10
Tangmouo Manfo, Brice <144279774+btmanfo@users.noreply.github.com>
2026-05-10 19:03:32 -0400

    Add: matchsticks-to-square - submission-2

---
+++ Data Structures & Algorithms/matchsticks-to-square/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        total = sum(matchsticks)
+        if total %4 != 0:
+            return False
+        sample = [0,0,0,0]
+
+        def backprapagation(i):
+            if i == len(matchsticks):
+                return sample[0] == sample[1] == sample[2] == sample[3]
+            for side in range(4):
+                sample[side] += matchsticks[i]
+                if backprapagation(i+1):
+                    return True
+                sample[side] -= matchsticks[i]
+            return False
+        return backprapagation(0)
